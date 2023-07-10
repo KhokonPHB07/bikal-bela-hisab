0cd266d9e6674eac5218b83a736cf07247ce7af9
KhokonPHB07 <kshehala@gmail.com>
2023-07-10 18:16:43 +0600

    making something

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Login from './Components/Login/Login'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl italic font-bold bg-orange-600 p-8 text-white'>Evening Fast</h1>
+      <Login></Login>
+    </>
+  )
+}
+
+export default App
